f02505d8aea0554ff164930732c4ddd34ab444f8
rajansinghbaliyan <rajansinghbaliyan@gmail.com>
2022-03-01 10:59:36 +0530

    featL remove the additional dependencies

--- src/main/java/com/cherrytechnologies/fibworker/repository/FibSeriesRepository.java
+++ src/main/java/com/cherrytechnologies/fibworker/repository/FibSeriesRepository.java
@@ -1,9 +1,9 @@
 package com.cherrytechnologies.fibworker.repository;
 
 import com.cherrytechnologies.fibworker.domain.FibSeries;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FibSeriesRepository extends JpaRepository<FibSeries,Integer> {
+public interface FibSeriesRepository extends CrudRepository<FibSeries,Integer> {
 }
